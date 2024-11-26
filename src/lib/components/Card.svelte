ca657043832983f1894351fe9e6613e5d554e8ce
Fantta8820 <andrevictor2512@gmail.com>
2024-11-26 19:57:04 -0300

    First commit

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,22 @@
+<div class="div">
+    <h1 class="text">Texto</h1>
+</div>
+
+
+<style>
+    .div{        
+        width: 100px;
+        height: 100px;
+        background-color: rgb(207, 207, 207);
+        border-color: black;
+        border-radius: 10%;
+        display: grid;
+        place-items: center;
+        
+    }
+
+    .text{        
+        text-align: center;
+        font-size: 20px;
+    }
+</style>
